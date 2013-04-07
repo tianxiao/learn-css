c0256a5867be5839df7acb786b1de2ae550f2af5
tianxiao <tianxiao987@gmail.com>
2013-04-07 10:01:47 +0800

    pack the code to make tidy

--- script/numkey.js
+++ script/numkey.js
@@ -1,9 +1,3 @@
-$(document).keydown(function(e) {
-	if (e.keycode == 37) {
-		alert("left pressed!");
-		return false;
-	}
-});
 
 // key:[up, down, left, right]
 //     [0   1     2     3]
@@ -35,7 +29,7 @@ var htmlkeymap = {
 };
 
 // var currentselected = keymap["tk1"];
-var currentselected = "tk5";
+var currentselected = "tk1";
 
 
 function keypressinit() {
@@ -46,19 +40,14 @@ function keypressinit() {
 	} else {
 		document.onkeydown = keypressalert;
 	}
+	
+	$(htmlkeymap[currentselected]).css("background","#99FFFF");
 }
 
 
+// output counter
+//var i=0;
 function keypressalert(e) {
-	if (!e) {
-		e = event;
-	}
-	
-	// alert(""+e.keyCode);
-	// $(#d3).html
-	var d3html = $("#d3").html();
-	if (!d3html) d3html="";
-	//showmesg(e.keyCode+":\t"+d3html);
 	
 	// temporary variable to currentposition;
 	var tempositioin = currentselected;
@@ -83,7 +72,7 @@ function keypressalert(e) {
 		break;
 	}
 	
-	if (direction!=null)  currentselected = direction;
+	currentselected = direction;
 	
 	showmesg(tempositioin+"-->"+currentselected );
 	
@@ -100,27 +89,7 @@ function keypressalert(e) {
 var lines = 0;
 var maxlines = 24;
 
-function init() {
-	document.testform.t.value += '';
-	lines = 0;
-	if (document.addEventListener) {
-		document.addEventListener("keydown", keydown, false);
-		document.addEventListener("keypress", keypress, false);
-		document.addEventListener("keyup", keyup, false);
-		document.addEventListener("textInput", textinput, false);
-	} else if (document.attachEvent) {
-		document.attachEvent("onkeydown", keydown);
-		document.attachEvent("onkeypress", keypress);
-		document.attachEvent("onkeyup", keyup);
-		document.attachEvent("ontextInput", textinput);
-	} else {
-		document.onkeydown = keydown;
-		document.onkeypress = keypress;
-		document.onkeyup = keyup;
-		document.ontextinput = textinput;
-		// probably doesn't work
-	}
-}
+
 
 function showmesg(t) {
 	var old = document.testform.t.value;
@@ -133,86 +102,3 @@ function showmesg(t) {
 	document.testform.t.value = old + t + '\n';
 }
 
-function keyval(n) {
-	if (n == null)
-		return 'undefined';
-	var s = pad(3, n);
-	if (n >= 32 && n < 127)
-		s += ' (' + String.fromCharCode(n) + ')';
-	while (s.length < 9)
-	s += ' ';
-	return s;
-}
-
-function keymesg(w, e) {
-	var row = 0;
-	var head = [w, ' '];
-	if (document.testform.classic.checked) {
-		showmesg(head[row] + ' keyCode=' + keyval(e.keyCode) + ' which=' + keyval(e.which) + ' charCode=' + keyval(e.charCode));
-		row = 1;
-	}
-
-	if (document.testform.modifiers.checked) {
-		showmesg(head[row] + ' shiftKey=' + pad(5, e.shiftKey) + ' ctrlKey=' + pad(5, e.ctrlKey) + ' altKey=' + pad(5, e.altKey) + ' metaKey=' + pad(5, e.metaKey));
-		row = 1;
-	}
-
-	if (document.testform.dom3.checked) {
-		showmesg(head[row] + ' key=' + e.key + ' char=' + e.char + ' location=' + e.location + ' repeat=' + e.repeat);
-		row = 1;
-	}
-
-	if (document.testform.olddom3.checked) {
-		showmesg(head[row] + ' keyIdentifier=' + pad(8, e.keyIdentifier) + ' keyLocation=' + e.keyLocation);
-		row = 1;
-	}
-
-	if (row == 0)
-		showmesg(head[row]);
-}
-
-function pad(n, s) {
-	s += '';
-	while (s.length < n)
-	s += ' ';
-	return s;
-}
-
-function suppressdefault(e, flag) {
-	if (flag) {
-		if (e.preventDefault)
-			e.preventDefault();
-		if (e.stopPropagation)
-			e.stopPropagation();
-	}
-	return !flag;
-}
-
-function keydown(e) {
-	if (!e)
-		e = event;
-	keymesg('keydown ', e);
-	return suppressdefault(e, document.testform.keydown.checked);
-}
-
-function keyup(e) {
-	if (!e)
-		e = event;
-	keymesg('keyup ', e);
-	return suppressdefault(e, document.testform.keyup.checked);
-}
-
-function keypress(e) {
-	if (!e)
-		e = event;
-	keymesg('keypress', e);
-	return suppressdefault(e, document.testform.keypress.checked);
-}
-
-function textinput(e) {
-	if (!e)
-		e = event;
-	//showmesg('textInput data=' + e.data);
-	showmesg('textInput data=' + e.data);
-	return suppressdefault(e, document.testform.textinput.checked);
-} 
